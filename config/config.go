8071fb6e055dd324b2ec0dcac7a62f756f7da50c
zgwldrc <zgwldrc@163.com>
2016-10-31 19:29:49 +0800

    config little change

--- config/config.go
+++ config/config.go
@@ -8,11 +8,9 @@ import (
 )
 
 
-func init(){
-    //定义一个salt, 用于混入原始密码，然后计算多次hash,更加不容易被cracker通过彩虹表泄漏原始密码
-    salt := "mysaltstring"
-
-    //定义一个存储配置结构体的map，分别存储开发，测试，生产的不同配置
+//该模块用于解析配置文件
+func init() {
+    //定义一个配置结构体的map，分别存储开发，测试，生产的不同配置
     var configMap = map[string]iris.Configuration{}
     //从配置文件加载配置数据
     cf, errOpen := os.Open(`config.json`)
@@ -34,14 +32,11 @@ func init(){
 
     //加载配置之后，先合并基础配置，再合并特定环境的配置
     configMap["BASIC"].Set(iris.Config)
+
     configMap["DEV"].Set(iris.Config)
     fmt.Println("########当前使用配置#########")
     fmt.Println("iris.Config.Gzip:",iris.Config.Gzip)
     fmt.Println("iris.Config.IsDevelopment:",iris.Config.IsDevelopment)
     fmt.Println(`iris.Config.Other["MySQLDSN"]:`,iris.Config.Other["MySQLDSN"])
-    iris.Config.Other["salt"] = salt
-    fmt.Println(`iris.Config.Other["salt"]:`,iris.Config.Other["salt"])
-
-
 }
 
