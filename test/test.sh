0e56ec4c976661fef7dfad1e57b8a402f82147a5
zgwldrc <zgwldrc@163.com>
2016-12-23 10:46:58 +0800

    mod

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-#登录
-curl localhost:8080/login -d'{"name":"admin","password":"admin"}' -i -c /tmp/cookie
+#admin 登陆
+curl -i -XPOST localhost:8080/login -d'{
+	"name":"admin","password":"admin"
+}' -c /tmp/cookie
 
-#新增用户
-curl -i -XPOST localhost:8080/user -d'{"Name":"xiayu","Password":"123"}' -b /tmp/cookie
-#删除用户
-curl -i -XDELETE localhost:8080/user/2 -d'{"Name":"xiayu"}' -b /tmp/cookie
 
-#获得帐户类型列表
-curl -i localhost:8080/account-type -b /tmp/cookie
 #新增一个账户信息
-curl -i XPOST localhost:8080/account -d'{}'
+curl -i -XPOST localhost:8080/account -d'{
+	"account": "zgwldrc@163.com",
+	"password": "test",
+	"endpoint": {
+		"url": "https://mail.163.com"
+	},
+	"account_type_id": 1
+}' -b /tmp/cookie
 
-#登出
-curl -XDELETE localhost:8080/login -i -b /tmp/cookie
+#新增一个账户信息
+curl -i -XPOST localhost:8080/account -d'{
+	"account": "zgwldrc@162.com",
+	"password": "test",
+	"endpoint": {
+		"url": "https://mail.162.com"
+	},
+	"account_type_id": 1
+}' -b /tmp/cookie
+
+#得到一个账户信息
+curl -i -XGET -b /tmp/cookie localhost:8080/account/1
+
+#得到我的账户列表
+curl -i -XGET -b /tmp/cookie localhost:8080/account
